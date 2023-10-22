86f71c357eda86a96da5724661cf195e7c4b6f8a
Pavel Urdin <p.urdin@g.nsu.ru>
2023-10-22 15:58:17 +0700

    Field: getNeighboursCount

--- C++-Labs/lab_2_game_of_life/src/core/Field.cpp
+++ C++-Labs/lab_2_game_of_life/src/core/Field.cpp
@@ -1,9 +1,47 @@
 #include "Field.h"
 
+#include <stdexcept>
+
 Field::Field(size_t width, size_t height)
- : field_data_(width, std::vector<bool>(height, false)){
+ : field_data_(validateSize(width), std::vector<bool>(validateSize(height), false)){
 }
 
 std::vector<bool> &Field::operator[](size_t x) {
     return field_data_[x];
+}
+
+uint32_t Field::getNeighboursCount(size_t x, size_t y) const {
+    size_t leftColumn = (x + this->width() - 1) % this->width();
+    size_t rightColumn = (x + 1) % this->width();
+    size_t topRow = (y + this->height() - 1) % this->height();
+    size_t bottomRow = (y + 1) % this->height();
+
+    uint32_t neighboursCount = 0;
+    neighboursCount += field_data_[leftColumn][topRow];
+    neighboursCount += field_data_[leftColumn][y];
+    neighboursCount += field_data_[leftColumn][bottomRow];
+
+    neighboursCount += field_data_[x][topRow];
+    neighboursCount += field_data_[x][bottomRow];
+
+    neighboursCount += field_data_[rightColumn][topRow];
+    neighboursCount += field_data_[rightColumn][y];
+    neighboursCount += field_data_[rightColumn][bottomRow];
+
+    return neighboursCount;
+}
+
+size_t Field::width() const {
+    return this->field_data_.size();
+}
+
+size_t Field::height() const {
+    return this->field_data_[0].size();
+}
+
+size_t Field::validateSize(size_t size) {
+    if (size < 3) {
+        throw std::invalid_argument("Field size must be >= 3");
+    }
+    return size;
 }
--- C++-Labs/lab_2_game_of_life/src/core/Field.h
+++ C++-Labs/lab_2_game_of_life/src/core/Field.h
@@ -7,14 +7,21 @@
 
 class Field {
 public:
+    Field() = delete;
     /// Create empty field with specified size
     Field(size_t width, size_t height);
 
     std::vector<bool> &operator[](size_t x);
 
+    /// Get cell neighbours count in Moore neighbourhood
+    [[nodiscard]] uint32_t getNeighboursCount(size_t x, size_t y) const;
+
+    [[nodiscard]] size_t width() const;
+    [[nodiscard]] size_t height() const;
+
 private:
     std::vector<std::vector<bool>> field_data_;
-
+    static size_t validateSize(size_t size);
 };
 
 #endif //LAB_2_GAME_OF_LIFE_FIELD_H
