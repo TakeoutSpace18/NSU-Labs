565b15a1e7ddfebcbd849a81426182d2dce9166e
Pavel Urdin <p.urdin@g.nsu.ru>
2023-11-04 15:54:54 +0700

    templated getOptions

--- C++-Labs/lab_2_game_of_life/src/utils/CommandLineArguments.cpp
+++ C++-Labs/lab_2_game_of_life/src/utils/CommandLineArguments.cpp
@@ -1,6 +1,7 @@
 #include "CommandLineArguments.h"
 
 #include <vector>
+#include <optional>
 
 #include "Logger.h"
 
@@ -8,7 +9,7 @@ CommandLineArguments::CommandLineArguments(int argc, char **argv) {
     std::vector<std::string_view> arguments;
     std::copy(argv, argv + argc, std::back_inserter(arguments));
 
-    int curr_arg= 0;
+    int curr_arg = 0;
     while (curr_arg < argc) {
         std::string_view name;
         std::string_view value;
@@ -24,17 +25,40 @@ CommandLineArguments::CommandLineArguments(int argc, char **argv) {
             continue;
         }
 
-        if (!arguments[curr_arg + 1].starts_with('-')) {
+        if (curr_arg < argc - 1 && !arguments[curr_arg + 1].starts_with('-')) {
             value = arguments[curr_arg + 1];
             curr_arg += 2;
         }
-
-        if (!options_.insert(std::make_pair(name, value)).second) {
-            logger << Logger::Info << "option \"" << name << "\" appeared multiple times\n";
+        else {
+            value = "True";
+            ++curr_arg;
         }
 
-        ++curr_arg;
+        if (!options_.insert(std::make_pair(name, value)).second) {
+            logger << Logger::Warning << "option \"" << name << "\" appeared multiple times\n";
+        }
     }
 
 
+}
+
+std::optional<std::string_view> CommandLineArguments::getOption(const std::string &option_name) {
+    auto found = options_.find(option_name);
+    if (found != options_.end()) {
+        return found->second;
+    }
+    return std::nullopt;
+}
+
+std::optional<std::string_view>
+CommandLineArguments::getOption(const std::string &option_name, const std::string &alias) {
+    auto full_named = getOption(option_name);
+    auto alias_named = getOption(alias);
+    if (full_named) {
+        return full_named;
+    }
+    if (alias_named) {
+        return alias_named;
+    }
+    return std::nullopt;
 }
--- C++-Labs/lab_2_game_of_life/src/utils/CommandLineArguments.h
+++ C++-Labs/lab_2_game_of_life/src/utils/CommandLineArguments.h
@@ -4,13 +4,47 @@
 
 #include <string>
 #include <map>
+#include <optional>
+#include <sstream>
+#include "Logger.h"
 
 class CommandLineArguments {
 public:
     CommandLineArguments(int argc, char *argv[]);
 
-    std::string getOption(const std::string &option_name);
-    std::string getOption(const std::string &option_name, const std::string &alias);
+    std::optional<std::string_view> getOption(const std::string &option_name);
+    std::optional<std::string_view> getOption(const std::string &option_name, const std::string &alias);
+
+    template<class T>
+    std::optional<T> getOption(const std::string &option_name) {
+        auto option = getOption(option_name);
+        if (option) {
+            T converted;
+            static std::istringstream stream;
+            stream.str(std::string(option.value()));
+            stream >> converted;
+            if (stream.fail()) {
+                logger << Logger::Error << "failed to convert option \"" << option_name
+                << "\" to " << typeid(T).name() << " type.\n";
+                stream.clear();
+            }
+            return converted;
+        }
+        return std::nullopt;
+    }
+
+    template<class T>
+    std::optional<T> getOption(const std::string &option_name, const std::string &alias) {
+        auto full_named = getOption<T>(option_name);
+        auto alias_named = getOption<T>(alias);
+        if (full_named) {
+            return full_named;
+        }
+        if (alias_named) {
+            return alias_named;
+        }
+        return std::nullopt;
+    }
 
 private:
     std::map<std::string_view, std::string_view> options_;
