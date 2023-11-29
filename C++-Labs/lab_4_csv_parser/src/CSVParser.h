98e1580e0c9c535b2fefd19775e04db159bc7861
Pavel Urdin <p.urdin@g.nsu.ru>
2023-11-29 17:27:31 +0700

    lab_4_csv_parser: parsing with custom separators draft

--- C++-Labs/lab_4_csv_parser/src/CSVParser.h
+++ C++-Labs/lab_4_csv_parser/src/CSVParser.h
@@ -12,25 +12,63 @@ public:
 
     struct Config {
         CharT column_delimiter = ',';
-        CharT newline_delimiter = '\n';
+        CharT line_delimiter = '\n';
         CharT escape_character = '\\';
     };
 
-    explicit basic_CSVParser(
-        std::basic_istream<CharT, Traits>&istream,
-        std::size_t skip_first_lines_count = 0,
-        Config config = Config())
-        : istream_(istream),
-          config_(config) {
+    void skipLines(std::size_t skip_first_lines_count) {
         for (auto i: std::views::iota(0ul, skip_first_lines_count)) {
-            istream_.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            istream_.ignore(std::numeric_limits<std::streamsize>::max(), config_.line_delimiter);
         }
     }
 
-    basic_CSVParser& operator>>(std::tuple<Types...>&tuple) {
-        std::basic_string<CharT, Traits> line;
-        std::getline(istream_, line, config_.newline_delimiter);
+    explicit basic_CSVParser(
+        std::basic_istream<CharT, Traits>&istream,
+        const std::size_t skip_first_lines_count = 0,
+        Config config = Config())
+        : istream_(istream),
+          config_(config) {
+        skipLines(skip_first_lines_count);
+    }
 
+    basic_CSVParser& operator>>(std::tuple<Types...>& tuple) {
+        std::basic_string<CharT, Traits> line;
+        std::getline(istream_, line, config_.line_delimiter);
+        std::vector<std::basic_string<CharT, Traits>> words;
+        bool last_delim_was_escaped = false;
+        for (size_t delim_idx = line.find(config_.column_delimiter);
+            delim_idx != std::string::npos;
+            delim_idx = line.find(config_.column_delimiter, delim_idx + 1))
+        {
+            if (line[delim_idx - 1] == config_.escape_character) {
+                auto line_part = line.substr(0, delim_idx - 1) + config_.column_delimiter;
+                if (last_delim_was_escaped) {
+                    words.back() += line_part;
+                }
+                else {
+                    words.push_back(line_part);
+                }
+                last_delim_was_escaped = true;
+            }
+            else { // word ends
+                auto line_part = line.substr(0, delim_idx);
+                if (last_delim_was_escaped) {
+                    words.back() += line_part;
+                }
+                else {
+                    words.push_back(line_part);
+                }
+                last_delim_was_escaped = false;
+            }
+            line = line.substr(delim_idx + 1);
+        }
+
+        if (words.size() != sizeof...(Types)) {
+            throw ParseError(current_line_number_, "columns number doesn't match");
+        }
+        
+        words.push_back(line);
+        ++current_line_number_;
         return *this;
     }
 
@@ -49,12 +87,17 @@ public:
 private:
     class ParseError : public std::runtime_error {
     public:
-        ParseError(const std::string&line, const std::string&message);
+        ParseError(const std::string& line, const std::string& message) :
+            runtime_error("Error while parsing line \"" + line + "\": " + message)
+        {}
     };
 
     std::basic_istream<CharT, Traits>&istream_;
     const Config config_;
+    size_t current_line_number_ = 0;
 };
+
+
 
 template<class CharT, class Traits, class... Types>
 struct basic_CSVParser<CharT, Traits, Types...>::InputIterator {
