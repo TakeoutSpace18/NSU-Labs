ea65f7e82c85bb53555365b5c70afbc398747e4a
Pavel Urdin <p.urdin@g.nsu.ru>
2023-12-13 00:17:19 +0700

    lab_9_cache_associativity

---
+++ EVM-Labs/lab_9_cache_associativity/src/CacheAssociativityMeasure.h
@@ -0,0 +1,37 @@
+#ifndef CACHESIZEMEASURE_H
+#define CACHESIZEMEASURE_H
+
+#include <chrono>
+#include <cstdint>
+
+class CacheAssociativityMeasure
+{
+public:
+    struct Config
+    {
+        std::size_t step_offset_bytes;
+        std::size_t max_banks;
+        std::size_t num_sets;
+    };
+
+    CacheAssociativityMeasure() = delete;
+
+    explicit CacheAssociativityMeasure(const Config &config);
+
+    ~CacheAssociativityMeasure();
+
+    void doMeasurings(const std::string &output_filename);
+
+private:
+
+    Config m_config;
+    volatile size_t* m_array;
+
+    void fillArray(std::size_t num_sets, std::size_t num_banks);
+    void traverseArray(std::size_t num_sets, std::size_t num_banks);
+
+    float measureTraverseTime(std::size_t num_sets, std::size_t num_banks, std::size_t traverse_repeats);
+};
+
+
+#endif //CACHESIZEMEASURE_H
