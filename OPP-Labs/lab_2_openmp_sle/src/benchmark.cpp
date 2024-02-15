d984b9c2de99f2071029570c981e9a60dcd9e375
Pavel Urdin <p.urdin@g.nsu.ru>
2024-02-15 21:11:53 +0700

    opp_lab_2_openmp: Plots

--- OPP-Labs/lab_2_openmp_sle/src/benchmark.cpp
+++ OPP-Labs/lab_2_openmp_sle/src/benchmark.cpp
@@ -2,8 +2,40 @@
 #include <cstdlib>
 #include <cstddef>
 #include <gnuplot-iostream.h>
+#include <omp.h>
+#include <filesystem>
+#include <vector>
 
 #include "SLESolver.h"
+
+constexpr SLESolver::DataType PRECISION = 1e-3;
+constexpr int MEASURE_REPEATS = 3;
+
+template<class F, class... Args>
+auto measureFunctionRuntime(F func, Args&&... args)
+{
+    using clock = std::chrono::high_resolution_clock;
+
+    const auto start = clock::now();
+    func(std::forward<Args>(args)...);
+    const auto stop = clock::now();
+    return stop - start;
+}
+
+template<class F, class... Args>
+auto measureFunctionRuntimeWithRepeats(int repeats, F func, Args&&... args)
+{
+    auto minTime = measureFunctionRuntime(func, std::forward<Args>(args)...);
+    for (int i = 1; i < repeats; ++i)
+    {
+        auto time = measureFunctionRuntime(func, std::forward<Args>(args)...);
+        if (time < minTime)
+        {
+            minTime = time;
+        }
+    }
+    return minTime;
+}
 
 std::vector<SLESolver::DataType> loadData(const std::string& filename, std::size_t size)
 {
@@ -13,27 +45,58 @@ std::vector<SLESolver::DataType> loadData(const std::string& filename, std::size
     {
         throw std::runtime_error("Failed to load data file " + filename);
     }
-    input.read(reinterpret_cast<char*>(loadedData.data()), sizeof(SLESolver::DataType) * size);
+    input.read(reinterpret_cast<char *>(loadedData.data()), sizeof(SLESolver::DataType) * size);
     return loadedData;
 }
 
 void saveData(const std::vector<SLESolver::DataType>& dataToSave, const std::string& filename)
 {
     std::ofstream output(filename, std::ios::binary);
-    output.write(reinterpret_cast<const char*>(dataToSave.data()), sizeof(SLESolver::DataType) * dataToSave.size());
+    output.write(reinterpret_cast<const char *>(dataToSave.data()), sizeof(SLESolver::DataType) * dataToSave.size());
 }
 
 int benchmark()
 {
-    std::size_t N = 50*50;
+    std::size_t N = 50 * 50;
+    int maxThreads = omp_get_max_threads();
 
-    auto matA = loadData("matA.bin", N*N);
+    std::vector<std::pair<int, int64_t>> threadsToTimeGraph(maxThreads);
+    std::vector<std::pair<int, int64_t>> accelerationGraph(maxThreads);
+    std::vector<std::pair<int, int64_t>> parallelizeEfficiencyGraph(maxThreads);
+
+    auto matA = loadData("matA.bin", N * N);
     auto vecB = loadData("vecB.bin", N);
 
     std::vector<SLESolver::DataType> foundVecX(N);
 
-    SLESolver::Solve(matA.data(), vecB.data(), foundVecX.data(), N, 1e-3);
+    for (int numThreads = 1; numThreads <= maxThreads; ++numThreads)
+    {
+        std::cout << "Computing with " << numThreads << " thread(s)... ";
+        std::cout.flush();
 
+        omp_set_num_threads(numThreads);
+        auto elapsedTime = measureFunctionRuntimeWithRepeats(MEASURE_REPEATS, SLESolver::Solve, matA.data(), vecB.data(), foundVecX.data(), N, PRECISION);
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(elapsedTime).count() << "ms\n";
+
+        threadsToTimeGraph.emplace_back(numThreads, elapsedTime.count());
+        accelerationGraph.emplace_back(numThreads, elapsedTime.count() / numThreads);
+        parallelizeEfficiencyGraph.emplace_back(numThreads, elapsedTime.count() / (numThreads * numThreads));
+    }
     saveData(foundVecX, "foundVecX.bin");
+
+
+    std::filesystem::create_directory("plot");
+    Gnuplot gp("tee plot/script.gp | gnuplot -persist");
+    gp << "set multiplot layout 2, 2\n";
+    gp << "plot" << gp.file1d(threadsToTimeGraph, "plot/threadsToTimeGraph.dat") << "with lines title 'raw time'\n";
+    gp << "plot" << gp.file1d(accelerationGraph, "plot/accelerationGraph.dat") << "with lines title 'acceleration'\n";
+    gp << "plot" << gp.file1d(parallelizeEfficiencyGraph, "plot/parallelizeEfficiencyGraph.dat") << "with lines title 'paralellize efficiency'\n";
+
     return EXIT_SUCCESS;
+}
+
+
+int main()
+{
+    return benchmark();
 }
--- OPP-Labs/lab_2_openmp_sle/test/visualize.py
+++ OPP-Labs/lab_2_openmp_sle/test/visualize.py
@@ -1,9 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 N = 50
 
-x = np.fromfile("vecX.bin", dtype=np.float32).reshape((N, N))
+vecXFilename = sys.argv[1] if len(sys.argv) > 1 else "vecX.bin"
+
+x = np.fromfile(vecXFilename, dtype=np.float32).reshape((N, N))
 b = np.fromfile("vecB.bin", dtype=np.float32).reshape((N, N))
 
 a = np.fromfile("matA.bin", dtype=np.float32).reshape((N*N, N*N))
@@ -11,7 +14,7 @@ a = np.fromfile("matA.bin", dtype=np.float32).reshape((N*N, N*N))
 fig, (ax1, ax2) = plt.subplots(ncols=2)
 
 ax1.imshow(x)
-ax1.set_title('Vector X')
+ax1.set_title(f'Vector X ({vecXFilename})')
 
 ax2.imshow(-b)
 ax2.set_title('Vector B')
