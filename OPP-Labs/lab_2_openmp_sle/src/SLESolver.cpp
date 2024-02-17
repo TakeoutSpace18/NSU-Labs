d3a97f8372173a23dc7c62fc3c8f0f027ba390f3
Pavel Urdin <p.urdin@g.nsu.ru>
2024-02-17 23:19:53 +0700

    opp_lab_2_openmp: external parallel directive

--- OPP-Labs/lab_2_openmp_sle/src/SLESolver.cpp
+++ OPP-Labs/lab_2_openmp_sle/src/SLESolver.cpp
@@ -5,6 +5,16 @@
 #include <iostream>
 #include <vector>
 
+// 1 - use #pragma omp parallel for
+// 0 - separate threads creation in external `#pragma omp parallel` directive
+#define OMP_PARALLEL_FOR 0
+
+// this helps to utilize intel P-cores while E-cores are still busy
+// #define SCHEDULE schedule(nonmonotonic:dynamic, 5)
+
+// however, best perfomance can be achived using only P-cores and static scheduling
+#define SCHEDULE schedule(static)
+
 const SLESolver::DataType SLESolver::sParam = -0.01;
 
 void SLESolver::Solve(const DataType* matA, const DataType* vecB, DataType* foundX, std::size_t N, DataType eps)
@@ -24,14 +34,18 @@ void SLESolver::solve(const DataType* matA, const DataType* vecB, DataType* foun
 
     // set initial X value
     std::fill_n(foundX, N, 0);
-
     do {
-        multiplyMatrixByVector(mMatA, foundX, vecAxMinusB.data(), N);
-        vectorSubtract(vecAxMinusB.data(), mVecB, N);
-
-        multiplyVectorByScalar(vecAxMinusB.data(), sParam, N);
-        vectorSubtract(foundX, vecAxMinusB.data(), N);
-    }
+        #if OMP_PARALLEL_FOR == 0
+        #pragma omp parallel
+        {
+        #endif
+            multiplyMatrixByVector(mMatA, foundX, vecAxMinusB.data(), N);
+            vectorSubtract(vecAxMinusB.data(), mVecB, N);
+            multiplyVectorByScalar(vecAxMinusB.data(), sParam, N);
+            vectorSubtract(foundX, vecAxMinusB.data(), N);}
+        #if OMP_PARALLEL_FOR == 0
+        }
+        #endif
     while (!checkPrecision(vecAxMinusB.data(), eps, N));
 }
 
@@ -39,7 +53,11 @@ void SLESolver::multiplyMatrixByVector(const DataType* mat, const DataType* vec,
 {
     std::fill_n(vecOut, N, 0);
 
-    #pragma omp parallel for
+    #if (OMP_PARALLEL_FOR == 1)
+        #pragma omp parallel for SCHEDULE
+    #else
+        #pragma omp for SCHEDULE
+    #endif
     for (std::size_t i = 0; i < N; ++i) // row
     {
         for (std::size_t j = 0; j < N; ++j) // col
@@ -51,7 +69,11 @@ void SLESolver::multiplyMatrixByVector(const DataType* mat, const DataType* vec,
 
 void SLESolver::multiplyVectorByScalar(DataType* vec, const DataType scalar, std::size_t N)
 {
-    #pragma omp parallel for
+    #if (OMP_PARALLEL_FOR == 1)
+        #pragma omp parallel for SCHEDULE
+    #else
+        #pragma omp for SCHEDULE
+    #endif
     for (std::size_t i = 0; i < N; ++i)
     {
         vec[i] *= scalar;
@@ -60,7 +82,11 @@ void SLESolver::multiplyVectorByScalar(DataType* vec, const DataType scalar, std
 
 void SLESolver::vectorSubtract(DataType* vecA, const DataType* vecB, std::size_t N)
 {
-    #pragma omp parallel for
+    #if (OMP_PARALLEL_FOR == 1)
+        #pragma omp parallel for SCHEDULE
+    #else
+        #pragma omp for SCHEDULE
+    #endif
     for (std::size_t i = 0; i < N; ++i)
     {
         vecA[i] -= vecB[i];
@@ -71,7 +97,11 @@ SLESolver::DataType SLESolver::vectorLength(const DataType* vec, std::size_t N)
 {
     DataType ret = 0;
 
-    #pragma omp parallel for reduction(+: ret)
+#if OMP_PARALLEL_FOR == 1
+    #pragma omp parallel for SCHEDULE reduction(+: ret)
+#else
+    #pragma omp for SCHEDULE
+#endif
     for (std::size_t i = 0; i < N; ++i)
     {
         ret += vec[i] * vec[i];
