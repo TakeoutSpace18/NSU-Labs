b77c5a5d65f441c790d129ffcc38c79e5965b8e0
Pavel Urdin <p.urdin@g.nsu.ru>
2024-04-09 15:00:29 +0700

    MPI lab3 matmul: time measuring

--- OPP-Labs/lab_3_mpi_matmul/MatrixMultiplier/src/main.cpp
+++ OPP-Labs/lab_3_mpi_matmul/MatrixMultiplier/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <mpi.h>
 
 #include "MatrixMultiplierMPI.h"
 
@@ -12,10 +14,25 @@ int main(int argc, char** argv)
 
     MPI::Init();
 
+    double beginTime = MPI::Wtime();
+
     MatrixMultiplierMPI multiplier(MPI::COMM_WORLD);
     std::array gridSize = {std::atoi(argv[7]), std::atoi(argv[8])};
     multiplier.MultiplyFromFiles(argv[1], argv[2], argv[3],
                                  std::atoi(argv[4]), std::atoi(argv[5]), std::atoi(argv[6]), gridSize);
+
+    double endTime = MPI::Wtime();
+    double timeElapsed = endTime - beginTime;
+    double maxTimeElapsed;
+    MPI::COMM_WORLD.Reduce(&timeElapsed, &maxTimeElapsed, 1, MPI::DOUBLE, MPI::MAX, 0);
+
+    int rank = MPI::COMM_WORLD.Get_rank();
+    if (rank == 0) {
+        std::cout << "Multiplication took " << maxTimeElapsed << " seconds\n";
+
+        std::ofstream out("timeElapsed.txt");
+        out << maxTimeElapsed;
+    }
 
     MPI::Finalize();
     return EXIT_FAILURE;
