f76f8873b3c460d88ee345fac9508677c8a2b452
Pavel Urdin <p.urdin@g.nsu.ru>
2024-05-19 15:20:44 +0700

    OS IPC: Fix bug with small shmem sizes

--- OS-Labs/6-ipc/task_1_shmem/shmem.c
+++ OS-Labs/6-ipc/task_1_shmem/shmem.c
@@ -8,13 +8,19 @@
 
 #define return_defer(code) do { status = (code); goto defer; } while (0);
 
-sem_t *semaphore = NULL;
+sem_t *written_semaphore = NULL;
+sem_t *chunk_read_semaphore = NULL;
 
 void write_sequence(unsigned int *array, size_t size)
 {
     for (unsigned int i = 0; i < UINT_MAX; ++i) {
+        if (i % size == 0) {
+            sem_wait(chunk_read_semaphore);
+        }
+
         array[i % size] = i;
-        sem_post(semaphore);
+        sem_post(written_semaphore);
+
         usleep(2000);
         if (i % 100 == 0) {
             printf("written %i numbers\n", i);
@@ -25,7 +31,11 @@ void write_sequence(unsigned int *array, size_t size)
 void check_sequence(const unsigned int *array, size_t size)
 {
     for (unsigned int i = 0; i < UINT_MAX; ++i) {
-        sem_wait(semaphore);
+        if (i % size == 0) {
+            sem_post(chunk_read_semaphore);
+        }
+
+        sem_wait(written_semaphore);
         if (array[i % size] != i) {
             printf("incorrect sequence at i=%i\n", i);
         }
@@ -40,7 +50,8 @@ int main()
 {
     int status = EXIT_SUCCESS;
 
-    size_t shmem_size = sysconf(_SC_PAGESIZE);
+    // size_t shmem_size = sysconf(_SC_PAGESIZE);
+    size_t shmem_size = 12;
 
     void *shmem = mmap(NULL, shmem_size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (shmem == MAP_FAILED) {
@@ -48,13 +59,16 @@ int main()
         return_defer(EXIT_FAILURE);
     }
 
-    semaphore = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (semaphore == MAP_FAILED) {
+    void *semaphores_mem = mmap(NULL, sizeof(sem_t) * 2, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (semaphores_mem == MAP_FAILED) {
         perror("mmap()");
         return_defer(EXIT_FAILURE);
     }
 
-    if (sem_init(semaphore, 1, 0) < 0) {
+    written_semaphore = semaphores_mem;
+    chunk_read_semaphore = semaphores_mem + sizeof(sem_t);
+
+    if (sem_init(written_semaphore, 1, 0) < 0 || sem_init(chunk_read_semaphore, 1, 0)) {
         perror("sem_init()");
         return_defer(EXIT_FAILURE);
     }
@@ -74,9 +88,9 @@ int main()
     }
 
 defer:
-    munmap(semaphore, sizeof(sem_t));
+    munmap(written_semaphore, sizeof(sem_t));
     munmap(shmem, shmem_size);
-    sem_destroy(semaphore);
+    sem_destroy(written_semaphore);
     wait(NULL);
     return status;
 }
