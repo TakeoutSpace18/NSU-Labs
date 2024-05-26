9d637bdb90ba4e6063f0eae325b4f40bc3c95e1f
Pavel Urdin <p.urdin@g.nsu.ru>
2024-05-26 22:05:59 +0700

    OS Networking: Task 1 UDP echo server

---
+++ OS-Labs/7-networking/task_1_udp_echo_server/udp_client.c
@@ -0,0 +1,55 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+#define BUFSIZE 64
+
+int main() {
+
+    int sfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sfd == -1) {
+        perror("socket()");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(6666);
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+
+    if (connect(sfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect()");
+        close(sfd);
+        return EXIT_FAILURE;
+    }
+
+    char buffer[BUFSIZE];
+    while (fgets(buffer, BUFSIZE, stdin) != NULL) {
+
+        if (send_n(sfd, buffer, strlen(buffer) + 1, 0) == -1) {
+            perror("send_n()");
+            close(sfd);
+            return EXIT_FAILURE;
+        }
+
+        if (recv_n(sfd, buffer, strlen(buffer) + 1, 0) == -1)
+        {
+            perror("recv_n()");
+            close(sfd);
+            return EXIT_FAILURE;
+        }
+
+        fputs(buffer, stdout);
+    }
+
+
+    return EXIT_SUCCESS;
+}
---
+++ OS-Labs/7-networking/task_1_udp_echo_server/udp_echo_server.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#include "utils.h"
+
+/* 64 KB is the max datagram size in UDP protocol */
+/* such size ensures that no data will be discarded */
+#define BUFSIZE 64 * 1024
+
+int main()
+{
+    int sfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sfd == -1) {
+        perror("socket()");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(6666);
+    server_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    if (bind(sfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind()");
+        close(sfd);
+        return EXIT_FAILURE;
+    }
+
+    char buffer[BUFSIZE];
+    while (true) {
+        struct sockaddr_in src_addr;
+        socklen_t src_addr_len = sizeof(src_addr);
+        ssize_t received_len = recvfrom(sfd, buffer, BUFSIZE, 0,  (struct sockaddr*)&src_addr, &src_addr_len);
+        if (received_len == -1) {
+            perror("recvfrom()");
+            close(sfd);
+            return EXIT_FAILURE;
+        }
+
+        char addr_text[20];
+        inet_ntop(AF_INET, &src_addr.sin_addr, addr_text, sizeof(addr_text));
+        printf("Received datagram from %s:%i\n", addr_text, ntohs(src_addr.sin_port));
+
+        if (sendto_n(sfd, buffer, received_len, 0, (struct sockaddr*)&src_addr, src_addr_len) == -1) {
+            perror("sendto_n()");
+            close(sfd);
+            return EXIT_FAILURE;
+        }
+    }
+
+
+    return EXIT_SUCCESS;
+}
