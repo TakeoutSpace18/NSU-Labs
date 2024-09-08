17750e9cf711c9447e0499136989276eb4db0c0a
Pavel Urdin <p.urdin@g.nsu.ru>
2024-09-08 16:24:51 +0700

    Networks: self_discovery working version

--- Networks/1-multicast_self_discovery/src/self_discovery.c
+++ Networks/1-multicast_self_discovery/src/self_discovery.c
@@ -1,52 +1,42 @@
+#include <asm-generic/socket.h>
+#include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>
 #include <unistd.h>
 
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 
-#define DEFAULT_PORT 8686
+const char *default_group_ip = "224.0.0.1";
+const char *default_port = "8686";
 
 #define IM_ALIVE_MSG 0xA11A
 #define UPDATE_TRIGGER_MSG 0xB22B
 
-typedef enum {
-    OK,
-    ERROR
-} Result_t;
+typedef enum { OK, ERROR } Result_t;
 
 #define ERR_DESCR_BUFSIZE 512
 static char err_descr[ERR_DESCR_BUFSIZE];
 
 static void
-print_usage()
-{
+print_usage() {
     printf("self_discovery <IPv4|IPv6 multicast group address> [port]\n");
 }
 
 static Result_t
-bind_ipv4(int sfd, char *host, char *port)
+socket_config(int sfd)
 {
-    struct sockaddr_in addr = { 0 };
-    addr.sin_family = AF_INET;
-    if (port)
-        addr.sin_port = htons(atoi(port));
-    else
-        addr.sin_port = htons(DEFAULT_PORT);
-
-    if (inet_pton(AF_INET, host, &addr.sin_addr) == 0) {
-        snprintf(err_descr, ERR_DESCR_BUFSIZE,
-                 "multicast address is invalid");
-        return ERROR;
-    }
-
-    int err = bind(sfd, (struct sockaddr *) &addr, sizeof(addr));
+    int reuse = 1;
+    int err = setsockopt(sfd, SOL_SOCKET, SO_REUSEADDR,
+                         (char *) &reuse, sizeof(reuse));
     if (err == -1) {
-        snprintf(err_descr, ERR_DESCR_BUFSIZE,
-                 "bind(): %s", strerror(errno));
+        snprintf(err_descr, ERR_DESCR_BUFSIZE, 
+                 "setsockopt(): %s", strerror(errno));
         return ERROR;
     }
 
@@ -54,25 +44,52 @@ bind_ipv4(int sfd, char *host, char *port)
 }
 
 static Result_t
-bind_ipv6(int sfd, char *host, char *port)
+bind_multicast_group(const char *ip, const char *port, int *sfd,
+                     struct sockaddr_storage *bound_addr,
+                     socklen_t *bound_addr_len)
 {
-    struct sockaddr_in6 addr = { 0 };
-    addr.sin6_family = AF_INET6;
-    if (port)
-        addr.sin6_port = htons(atoi(port));
-    else
-        addr.sin6_port = htons(DEFAULT_PORT);
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
 
-    if (inet_pton(AF_INET6, host, &addr.sin6_addr) == 0) {
+    struct addrinfo* result;
+
+    int err = getaddrinfo(ip, port, &hints, &result);
+    if (err != 0) {
         snprintf(err_descr, ERR_DESCR_BUFSIZE,
-                 "multicast address is invalid");
+                 "getaddrinfo(): %s", gai_strerror(err));
         return ERROR;
     }
 
-    int err = bind(sfd, (struct sockaddr *) &addr, sizeof(addr));
-    if (err == -1) {
-        snprintf(err_descr, ERR_DESCR_BUFSIZE,
-                 "bind(): %s", strerror(errno));
+    struct addrinfo *rp; 
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        *sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (*sfd == -1)
+            continue;
+
+        Result_t ret = socket_config(*sfd);
+        if (ret != OK) {
+            close(*sfd);
+            continue;
+        }
+
+        if (bind(*sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+            memcpy(bound_addr, rp->ai_addr, rp->ai_addrlen);
+            *bound_addr_len = rp->ai_addrlen;
+            break; /* Success */
+        }
+
+        close(*sfd);
+    }
+
+    freeaddrinfo(result);
+
+    if (rp == NULL) {
+        snprintf(err_descr, ERR_DESCR_BUFSIZE, 
+                 "Could not bind: %s", strerror(errno));
         return ERROR;
     }
 
@@ -80,113 +97,118 @@ bind_ipv6(int sfd, char *host, char *port)
 }
 
 static Result_t
-broadcast_message(int sockfd, int msg)
-{
+send_message(int sockfd, int msg,
+             struct sockaddr *dest_addr, socklen_t dest_addr_len) {
     ssize_t sent = 0;
     while (sent < sizeof(msg)) {
-        ssize_t ret = write(sockfd, (char *) &msg + sent, sizeof(msg) - sent);
+        ssize_t ret = sendto(sockfd, (char*)&msg + sent, sizeof(msg) - sent,
+                             0, dest_addr, dest_addr_len);
         if (ret == -1) {
-            snprintf(err_descr, ERR_DESCR_BUFSIZE, 
-                     "write(): %s", strerror(errno));
+            snprintf(err_descr, ERR_DESCR_BUFSIZE, "sendto(): %s",
+                     strerror(errno));
             return ERROR;
         }
+
+        sent += ret;
     }
 
     return OK;
 }
 
 static Result_t
-recieve_message(int sockfd, int *msg,
-                struct sockaddr *src_addr, socklen_t *addrlen)
-{
+recieve_message(int sockfd, int* msg, struct sockaddr* src_addr,
+                                socklen_t* addrlen) {
     ssize_t recieved = 0;
     while (recieved < sizeof(int)) {
-        ssize_t ret = recvfrom(sockfd, (char *) msg + recieved,
+        ssize_t ret = recvfrom(sockfd, (char*)msg + recieved,
                                sizeof(int) - recieved, 0, src_addr, addrlen);
         if (ret == -1) {
-            snprintf(err_descr, ERR_DESCR_BUFSIZE, 
-                     "recvfrom(): %s", strerror(errno));
+            snprintf(err_descr, ERR_DESCR_BUFSIZE, "recvfrom(): %s",
+                     strerror(errno));
             return ERROR;
         }
+
+        recieved += ret;
     }
 
     return OK;
 }
 
-int
-main(int argc, char **argv)
+static Result_t
+print_app_instance_info(struct sockaddr *addr, socklen_t addrlen)
 {
-    if (argc < 2 || argc > 3) {
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    int ret = getnameinfo(addr, addrlen, host, NI_MAXHOST,
+                          service, NI_MAXSERV, 0);
+
+    if (ret != 0) {
+        snprintf(err_descr, ERR_DESCR_BUFSIZE,
+                 "getnameinfo(): %s", gai_strerror(ret));
+        return ERROR;
+    }
+
+    printf("%s, %s is alive\n", host, service);
+
+    return OK;
+}
+
+int main(int argc, char** argv) {
+    if (argc > 3) {
         fprintf(stderr, "wrong arguments count!\n");
         print_usage();
         return EXIT_FAILURE;
     }
 
-    char *multicast_addr = argv[1];
+    const char* group_ip;
+    const char* port;
 
-    char *port;
-    if (argc == 3)
+    if (argc == 3) {
+        group_ip = argv[1];
         port = argv[2];
-    else
-        port = NULL;
-
-    int ipmode;
-    if (strchr(multicast_addr, '.'))
-        ipmode = AF_INET;
-    else
-        ipmode = AF_INET6;
-
-    int sfd;
-    sfd = socket(ipmode, SOCK_DGRAM, 0);
-
-    if (sfd < 0) {
-        perror("socket()");
-        return EXIT_FAILURE;
-    } 
-
-    Result_t ret;
-    switch (ipmode) {
-        case AF_INET:
-            ret = bind_ipv4(sfd, multicast_addr, port);
-            break;
-        case AF_INET6:
-            ret = bind_ipv6(sfd, multicast_addr, port);
-            break;
+    } else if (argc == 2) {
+        group_ip = argv[1];
+        port = default_port;
+    } else {
+        group_ip = default_group_ip;
+        port = default_port;
     }
 
+    int                     sfd;
+    struct sockaddr_storage groupaddr;
+    socklen_t               groupaddr_len;
+
+    Result_t ret = bind_multicast_group(group_ip, port, &sfd,
+                                        &groupaddr, &groupaddr_len);
     if (ret != OK) {
         fputs(err_descr, stderr);
         return EXIT_FAILURE;
     }
-
+ 
     /* Notify other instances of the app of your appearance 
      * and ask them to broadcast their state */
-    ret = broadcast_message(sfd, UPDATE_TRIGGER_MSG);
+    ret = send_message(sfd, UPDATE_TRIGGER_MSG,
+                       (struct sockaddr *) &groupaddr, groupaddr_len);
     if (ret != OK) {
         fputs(err_descr, stderr);
         return EXIT_FAILURE;
     }
 
     for (;;) {
-        int msg;
-        struct sockaddr *src = malloc(ipmode == AF_INET ?
-                                      sizeof(struct sockaddr_in) :
-                                      sizeof(struct sockaddr_in6));
+        int                     msg;
+        struct sockaddr_storage src;
+        socklen_t               srclen;
 
-        if (!src) {
-            fputs("out of memory\n", stderr);
-            abort();
-        }
-
-        socklen_t srclen;
-        ret = recieve_message(sfd, &msg, src, &srclen);
+        ret = recieve_message(sfd, &msg, (struct sockaddr *) &src, &srclen);
         if (ret != OK) {
             fputs(err_descr, stderr);
             return EXIT_FAILURE;
         }
 
         if (msg == UPDATE_TRIGGER_MSG) {
-            ret = broadcast_message(sfd, IM_ALIVE_MSG);
+            ret = send_message(sfd, IM_ALIVE_MSG,
+                               (struct sockaddr *) &groupaddr, groupaddr_len);
             if (ret != OK) {
                 fputs(err_descr, stderr);
                 return EXIT_FAILURE;
@@ -194,13 +216,11 @@ main(int argc, char **argv)
         }
 
         if (msg == IM_ALIVE_MSG) {
-            char addrbuf[64];
-            if (!inet_ntop(ipmode, src, addrbuf, sizeof(addrbuf))) {
-                perror("inet_ntop()");
-                return EXIT_FAILURE;
+            Result_t ret = print_app_instance_info((struct sockaddr *) &src,
+                                                   srclen);
+            if (ret != OK) {
+                fputs(err_descr, stderr);
             }
-
-            printf("%s is alive\n", addrbuf);
         }
     }
 }
