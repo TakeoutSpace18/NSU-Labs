e004e75a73ad5975153a029a954d976c80c03496
Pavel Urdin <p.urdin@g.nsu.ru>
2024-09-11 22:44:53 +0700

    Networks: self_discovery logic rework

--- Networks/1-multicast_self_discovery/src/self_discovery.c
+++ Networks/1-multicast_self_discovery/src/self_discovery.c
@@ -1,11 +1,13 @@
 #include <asm-generic/socket.h>
+#include <bits/types/struct_timeval.h>
 #include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
+#include <unistd.h>
+#include <signal.h>
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -18,14 +20,36 @@ const char *default_port = "8686";
 #define IM_ALIVE_MSG 0xA11A
 #define UPDATE_TRIGGER_MSG 0xB22B
 
-typedef enum { OK, ERROR } Result_t;
+typedef enum { OK, ERROR, TIMED_OUT } Result_t;
 
 #define ERR_DESCR_BUFSIZE 512
 static char err_descr[ERR_DESCR_BUFSIZE];
 
+static bool is_interrupted = false;
+
 static void
 print_usage() {
     printf("self_discovery <IPv4|IPv6 multicast group address> [port]\n");
+}
+
+static void
+sigint_handler(int signo)
+{
+    is_interrupted = true;
+}
+
+static Result_t
+setup_signal_handlers()
+{
+    struct sigaction sigint_sa;
+    memset(&sigint_sa, 0, sizeof(sigint_sa));
+    sigint_sa.sa_handler = sigint_handler;
+    if (sigaction(SIGINT, &sigint_sa, NULL) != 0) {
+        snprintf(err_descr, ERR_DESCR_BUFSIZE, 
+                 "sigaction(): %s", strerror(errno));
+        return ERROR;
+    }
+
 }
 
 static Result_t
@@ -122,6 +146,14 @@ recieve_message(int sockfd, int* msg, struct sockaddr* src_addr,
     while (recieved < sizeof(int)) {
         ssize_t ret = recvfrom(sockfd, (char*)msg + recieved,
                                sizeof(int) - recieved, 0, src_addr, addrlen);
+        if (ret == -1 && errno == EAGAIN)
+            return TIMED_OUT;
+
+        if (ret == -1 && errno == EINTR) {
+            is_interrupted = true;
+            return OK;
+        }
+
         if (ret == -1) {
             snprintf(err_descr, ERR_DESCR_BUFSIZE, "recvfrom(): %s",
                      strerror(errno));
@@ -138,23 +170,79 @@ static Result_t
 print_app_instance_info(struct sockaddr *addr, socklen_t addrlen)
 {
     char host[NI_MAXHOST];
+    char numeric_host[NI_MAXHOST];
     char service[NI_MAXSERV];
 
-    int ret = getnameinfo(addr, addrlen, host, NI_MAXHOST,
-                          service, NI_MAXSERV, 0);
+    int ret = getnameinfo(addr, addrlen, numeric_host, NI_MAXHOST,
+                          service, NI_MAXSERV,
+                          NI_NUMERICSERV | NI_NUMERICHOST);
+    if (ret == -1)
+        goto getnameinfo_fail;
 
-    if (ret != 0) {
+    ret = getnameinfo(addr, addrlen, host, NI_MAXHOST, NULL, 0, 0);
+    if (ret == -1)
+        goto getnameinfo_fail;
+
+
+    printf("%s (%s), port %s is alive\n", host, numeric_host, service);
+    return OK;
+
+getnameinfo_fail:
         snprintf(err_descr, ERR_DESCR_BUFSIZE,
                  "getnameinfo(): %s", gai_strerror(ret));
         return ERROR;
-    }
-
-    printf("%s, %s is alive\n", host, service);
-
-    return OK;
 }
 
+static void
+disable_rcv_timeout(int sfd) {
+    struct timeval time = { 0 };
+    setsockopt(sfd, SOL_SOCKET, SO_RCVTIMEO, &time, sizeof(time));
+}
+
+static Result_t
+collect_app_instances_info(int sfd, struct timeval timeout) 
+{
+    int ret = setsockopt(sfd, SOL_SOCKET, SO_RCVTIMEO,
+                         &timeout, sizeof(timeout));
+    if (ret == -1) {
+        snprintf(err_descr, ERR_DESCR_BUFSIZE,
+                 "setsockopt(): %s", gai_strerror(ret));
+        return ERROR;
+    }
+
+    printf("------------ ALIVE INSTANCES ------------\n");
+
+    for (;;) {
+        int                     msg;
+        struct sockaddr_storage src;
+        socklen_t               srclen;
+
+        Result_t ret = recieve_message(sfd, &msg,
+                                       (struct sockaddr *) &src, &srclen);
+        if (ret == TIMED_OUT)
+            break;
+
+        if (ret == ERROR)
+            goto error;
+
+        if (msg == IM_ALIVE_MSG) {
+            if (print_app_instance_info((struct sockaddr *) &src, srclen) != OK)
+                goto error;
+        }
+    }
+
+    disable_rcv_timeout(sfd);
+    return OK;
+
+error:
+    disable_rcv_timeout(sfd);
+    return ERROR;
+}
+
+
 int main(int argc, char** argv) {
+    Result_t ret;
+
     if (argc > 3) {
         fprintf(stderr, "wrong arguments count!\n");
         print_usage();
@@ -175,52 +263,65 @@ int main(int argc, char** argv) {
         port = default_port;
     }
 
+    ret = setup_signal_handlers();
+    if (ret != OK)
+        goto error;
+
     int                     sfd;
     struct sockaddr_storage groupaddr;
     socklen_t               groupaddr_len;
 
-    Result_t ret = bind_multicast_group(group_ip, port, &sfd,
-                                        &groupaddr, &groupaddr_len);
-    if (ret != OK) {
-        fputs(err_descr, stderr);
-        return EXIT_FAILURE;
-    }
+    ret = bind_multicast_group(group_ip, port, &sfd,
+                               &groupaddr, &groupaddr_len);
+    if (ret != OK)
+        goto error;
  
-    /* Notify other instances of the app of your appearance 
-     * and ask them to broadcast their state */
+    /* Ask all instances of the app to broadcast their state
+     * (this instance itself gets this message too)*/
     ret = send_message(sfd, UPDATE_TRIGGER_MSG,
                        (struct sockaddr *) &groupaddr, groupaddr_len);
-    if (ret != OK) {
-        fputs(err_descr, stderr);
-        return EXIT_FAILURE;
-    }
+    if (ret != OK)
+        goto error;
 
-    for (;;) {
+    while (!is_interrupted) {
         int                     msg;
         struct sockaddr_storage src;
         socklen_t               srclen;
 
         ret = recieve_message(sfd, &msg, (struct sockaddr *) &src, &srclen);
-        if (ret != OK) {
-            fputs(err_descr, stderr);
-            return EXIT_FAILURE;
-        }
+        if (ret != OK)
+            goto error;
 
         if (msg == UPDATE_TRIGGER_MSG) {
             ret = send_message(sfd, IM_ALIVE_MSG,
                                (struct sockaddr *) &groupaddr, groupaddr_len);
-            if (ret != OK) {
-                fputs(err_descr, stderr);
-                return EXIT_FAILURE;
-            }
-        }
+            if (ret != OK)
+                goto error;
 
-        if (msg == IM_ALIVE_MSG) {
-            Result_t ret = print_app_instance_info((struct sockaddr *) &src,
-                                                   srclen);
-            if (ret != OK) {
+            struct timeval collect_timeout;
+            collect_timeout.tv_sec = 0;
+            collect_timeout.tv_usec = 50000;
+            ret = collect_app_instances_info(sfd, collect_timeout);
+
+            if (ret != OK)
                 fputs(err_descr, stderr);
-            }
-        }
+
+        } 
     }
+
+    printf("Finishing...");
+
+    /* Notify other instances of your death */
+    ret = send_message(sfd, UPDATE_TRIGGER_MSG,
+                       (struct sockaddr *) &groupaddr, groupaddr_len);
+    if (ret != OK)
+        goto error;
+
+    close(sfd);
+    return EXIT_SUCCESS;
+
+error:
+    fputs(err_descr, stderr);
+    close(sfd);
+    return EXIT_FAILURE;
 }
