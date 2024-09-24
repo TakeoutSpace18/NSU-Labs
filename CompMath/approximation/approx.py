e7588d31ccf517842d0fcabb15a34cf6206ca162
Pavel Urdin <p.urdin@g.nsu.ru>
2024-09-24 15:35:41 +0700

    CompMath approximation: discontinuous function

--- CompMath/approximation/approx.py
+++ CompMath/approximation/approx.py
@@ -5,10 +5,9 @@ import numpy as np
 class CubicSpline:
     def __init__(self, x : list[float], y : list[float]):
         n = len(x)
-        self.__x = x
-        self.__y = y
+        self.x_samples = x
 
-        h = [0] + [(x[i] - x[i-1]) for i in range(1, n)]
+        h = [0.0] + [(x[i] - x[i-1]) for i in range(1, n)]
 
         a = [h[i] for i in range(1, n-1)]
         b = [2 * (h[i] + h[i+1]) for i in range(1, n-1)]
@@ -16,9 +15,9 @@ class CubicSpline:
 
         f = [6 * ((y[i+1] - y[i]) / h[i+1] - (y[i] - y[i-1]) / h[i]) for i in range(1, n-1)]
 
-        self.c_coefs = [0] + solve_tridiagonal_sle(a, b, c, f) + [0]
-        self.d_coefs = [0] + [(self.c_coefs[i] - self.c_coefs[i-1]) / h[i] for i in range(1, n)]
-        self.b_coefs = [0] + [None] * (n-1)
+        self.c_coefs = [0.0] + solve_tridiagonal_sle(a, b, c, f) + [0.0]
+        self.d_coefs = [0.0] + [(self.c_coefs[i] - self.c_coefs[i-1]) / h[i] for i in range(1, n)]
+        self.b_coefs = [0.0] + [None] * (n-1)
         for i in range(1, n):
             self.b_coefs[i] = h[i] * self.c_coefs[i] / 2
             self.b_coefs[i] -= (h[i] ** 2) * self.d_coefs[i] / 6
@@ -29,9 +28,11 @@ class CubicSpline:
     @np.vectorize
     def __call__(self, x : float):
         # find interval to which x belongs
-        i = np.searchsorted(self.__x, [x], side='left')[0]
+        i = np.searchsorted(self.x_samples, [x], side='left')[0]
 
-        return self.a_coefs[i] + self.b_coefs[i] * (x - self.__x[i]) + self.c_coefs[i] / 2 * (x - self.__x[i]) ** 2 + self.d_coefs[i] / 6 * (x - self.__x[i]) ** 3
+        return self.a_coefs[i] + self.b_coefs[i] * (x - self.x_samples[i]) \
+                + self.c_coefs[i] / 2 * (x - self.x_samples[i]) ** 2 \
+                + self.d_coefs[i] / 6 * (x - self.x_samples[i]) ** 3
 
 
 class NewtonPolynomial:
@@ -40,14 +41,14 @@ class NewtonPolynomial:
         for i in range(len(x)):
             self.__divided_diffs += [divided_difference(x[0:i+1], y[0:i+1])]
 
-        self.__x = x
-        self.__n = len(x)
+        self.x_samples = x
+        self.n = len(x)
 
     @np.vectorize
     def __call__(self, x : float) -> float:
         ret = 0
-        for i in range(self.__n):
-            p = np.prod([x - self.__x[j] for j in range(i)])
+        for i in range(self.n):
+            p = np.prod([x - self.x_samples[j] for j in range(i)])
             ret += self.__divided_diffs[i] * p;
 
         return ret
@@ -81,27 +82,35 @@ def divided_difference(x : list[float], y : list[float]) -> float:
     return ret
 
 
-if __name__ == "__main__":
-    x = np.linspace(-1, 1, 200)  # Sample data.
+@np.vectorize
+def f(x):
+    return np.sin(x) if x < 0 else np.sin(x) + 1
 
-    plt.xlabel('x label')
-    plt.ylabel('y label')
+if __name__ == "__main__":
+    x_start = -np.pi
+    x_stop = np.pi
+
+    x = np.linspace(x_start, x_stop, 200)  # Sample data.
+
+    plt.xlabel('x')
+    plt.ylabel('y')
     plt.title("Approximation")
-    plt.xlim(-1, 1)
-    plt.ylim(-0.5, 1.5)
+    plt.xlim(x_start, x_stop)
+    plt.ylim(x_start, x_stop)
 
     # Plot original function
-    plt.plot(x, abs(x), label='y = |x|')
+    # plt.plot(x, abs(x), label='y = |x|')
+    plt.plot(x, f(x), label='y = sin(x)(x < 0) + (sin(x) + 1)(x >= 0)')
 
     # Plot Newton polynomials
     for n in [11, 22, 44]:
-        x1 = np.linspace(-1, 1, n)
-        y1 = abs(x1)
+        x_points = np.linspace(x_start, x_stop, n)
+        f_points = f(x_points)
 
-        poly = NewtonPolynomial(x1, y1)
+        poly = NewtonPolynomial(x_points, f_points)
         plt.plot(x, poly(poly, x), label= f"NewtonPolynomial (n={n})")
 
-        spline = CubicSpline(x1, y1)
+        spline = CubicSpline(x_points, f_points)
         plt.plot(x, spline(spline, x), label= f"CubicSpline (n={n})")
 
     plt.legend()
