0020ef78fb778f801e6af60ab73a859c790c3007
Pavel Urdin <p.urdin@g.nsu.ru>
2025-03-17 16:01:41 +0700

    Epsmim: separate gui and benchmark mode, increase matrix size

--- Epsmim/src/main.cpp
+++ Epsmim/src/main.cpp
@@ -1,6 +1,8 @@
 #include <QApplication>
 
+#include <cstdlib>
 #include <qcoreapplication.h>
+#include <qnamespace.h>
 #include <random>
 #include <memory>
 #include <chrono>
@@ -8,10 +10,43 @@
 #include "Application.h"
 #include "WaveEquation.h"
 
-#define RANDOM_SOURCE_LOCATION 0
-
-int main(int argc, char* argv[])
+int benchmark_mode(int argc, char* argv[])
 {
+    WaveEquation::AreaParams area = {
+        .xa = 0.0,
+        .xb = 4.0,
+        .ya = 0.0,
+        .yb = 4.0,
+        .nx = 8000,
+        .ny = 8000
+    };
+
+    Utils::Vec2i source = {
+        .x = area.nx * 3 / 4,
+        .y = area.ny * 3 / 4
+    };
+
+    auto equation = std::make_unique<WaveEquation>(area, source);
+
+    using namespace std::chrono;
+
+    auto start = high_resolution_clock::now();
+    equation->skipNIterarions(100);
+    auto end = high_resolution_clock::now();
+
+    uint64_t elapsed = duration_cast<milliseconds>(end - start).count();
+    std::cout << "time: " << elapsed << "\n";
+
+    WaveEquation::Output out = equation->getCurrentState();
+    size_t size = area.nx * area.ny * sizeof(WaveEquation::ValueType);
+    Utils::writeToFile("out.dat", reinterpret_cast<const char*>(out.data), size);
+
+    return EXIT_SUCCESS;
+}
+
+int gui_mode(int argc, char* argv[])
+{
+
     WaveEquation::AreaParams area = {
         .xa = 0.0,
         .xb = 4.0,
@@ -23,43 +58,30 @@ int main(int argc, char* argv[])
 
     Utils::Vec2i source;
 
-    if (RANDOM_SOURCE_LOCATION) {
-        std::random_device rd;
-        std::default_random_engine eng(rd());
-        std::uniform_int_distribution<int> distr_x(0, area.nx);
-        std::uniform_int_distribution<int> distr_y(0, area.ny);
+    std::random_device rd;
+    std::default_random_engine eng(rd());
+    std::uniform_int_distribution<int> distr_x(0, area.nx);
+    std::uniform_int_distribution<int> distr_y(0, area.ny);
 
-        source = {
-            .x = distr_x(eng),
-            .y = distr_y(eng)
-        };
-    }
-    else {
-        source = {
-            .x = area.nx * 3 / 4,
-            .y = area.ny * 3 / 4
-        };
-    }
+    source = {
+        .x = distr_x(eng),
+        .y = distr_y(eng)
+    };
 
     auto equation = std::make_unique<WaveEquation>(area, source);
 
+    QApplication app(argc, argv);
+    Application window(std::move(equation));
+    window.show();
+    return app.exec();
+}
+
+int main(int argc, char* argv[])
+{
     if (argc == 2 && strcmp(argv[1], "--gui") == 0) {
-        QApplication app(argc, argv);
-        Application window(std::move(equation));
-        window.show();
-        return app.exec();
+        return gui_mode(argc, argv);
     }
-
-    using namespace std::chrono;
-
-    auto start = high_resolution_clock::now();
-    equation->skipNIterarions(1500);
-    auto end = high_resolution_clock::now();
-
-    uint64_t elapsed = duration_cast<milliseconds>(end - start).count();
-    std::cout << "time: " << elapsed << "\n";
-
-    WaveEquation::Output out = equation->getCurrentState();
-    size_t size = area.nx * area.ny * sizeof(WaveEquation::ValueType);
-    Utils::writeToFile("out.dat", reinterpret_cast<const char*>(out.data), size);
+    else {
+        return benchmark_mode(argc, argv);
+    }
 }
